b21074829a8f5135ad1acfa3125c758561efcfee
SAlejandr <alejitocastaneda1@gmail.com>
2021-10-20 17:43:42 +0200

    Mejora

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,5 +1,23 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.pojos.Producto;
+
+@Controller
 public class MainController {
+	
+	@GetMapping(value = "/indesx")
+	public String index(Model Modelo) {
+		
+		return "index";
+	}
+
+	
 
 }
