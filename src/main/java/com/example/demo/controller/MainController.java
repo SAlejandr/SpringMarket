d46486f2c42731ee22c0e8208060bbf1143fbeb0
SAlejandr <alejitocastaneda1@gmail.com>
2021-10-21 19:39:08 +0200

    A medias el controlador

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,16 +1,23 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.pojos.Producto;
+import com.example.demo.repository.ProductoDao;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private ProductoDao dao;
 	
 	@GetMapping(value = "/indesx")
 	public String index(Model modelo) {
@@ -22,6 +29,28 @@ public class MainController {
 	public String guardarProd(Model modelo) {
 		
 		return "formulario";
+	}
+
+	
+	@PostMapping(value = "/formula")
+	public String guardarProducto(Model modelo, @RequestParam String nombre, @RequestParam String descripcion, 
+			@RequestParam float precio, @RequestParam int descuento ) {
+		
+		Producto producto = new Producto(nombre, descripcion, precio, descuento);
+		
+		dao.save(producto);
+		
+		return "redirect: formula";
+	}
+
+	
+	@GetMapping(value = "/producto/{id}")
+	public String buscarProductoPorId(Model modelo, @PathVariable long id) {
+		
+		Optional<Producto> optional = dao.findById(id);
+		
+		
+		return new String();
 	}
 
 	
