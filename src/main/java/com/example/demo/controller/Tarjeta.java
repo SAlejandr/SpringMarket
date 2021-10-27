9657adeb436addbab87b6167085b155540eb95ef
SAlejandr <alejitocastaneda1@gmail.com>
2021-10-27 19:34:25 +0200

    Empiezo Iteracion 2

---
+++ src/main/java/com/example/demo/controller/Tarjeta.java
@@ -0,0 +1,82 @@
+package com.example.demo.controller;
+
+import java.math.BigInteger;
+import java.util.Objects;
+
+public class Tarjeta {
+	//variables
+	private BigInteger numero;
+	private String titular;
+	private int codSeguridad;
+	private String dirFactura;
+	//constructores
+	public Tarjeta() {
+		// TODO Auto-generated constructor stub
+		this(new BigInteger("0"),"",0,"");
+	}
+
+	public Tarjeta(BigInteger numero, String titular, int codSeguridad, String dirFactura) {
+		super();
+		this.numero = numero;
+		this.titular = titular;
+		this.codSeguridad = codSeguridad;
+		this.dirFactura = dirFactura;
+	}
+	//setter y Getter
+	public BigInteger getNumero() {
+		return numero;
+	}
+
+	public void setNumero(BigInteger numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public int getCodSeguridad() {
+		return codSeguridad;
+	}
+
+	public void setCodSeguridad(int codSeguridad) {
+		this.codSeguridad = codSeguridad;
+	}
+
+	public String getDirFactura() {
+		return dirFactura;
+	}
+
+	public void setDirFactura(String dirFactura) {
+		this.dirFactura = dirFactura;
+	}
+	//equals y hashCode
+	@Override
+	public int hashCode() {
+		return Objects.hash(numero);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarjeta other = (Tarjeta) obj;
+		return Objects.equals(numero, other.numero);
+	}
+	//toString
+	@Override
+	public String toString() {
+		return "Tarjeta [numero=" + numero + ", titular=" + titular + ", codSeguridad=" + codSeguridad + ", dirFactura="
+				+ dirFactura + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/example/demo/controller/Usuario.java
@@ -0,0 +1,112 @@
+package com.example.demo.controller;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Usuario {
+	//vairables
+	private long id;
+	private String nombre;
+	private String apellido;
+	private String contrasenna;
+	private String email;
+	private LocalDate fNacimiento;
+	//contuctores
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+		this("", "", "", "", LocalDate.now());
+	}
+
+	public Usuario(String nombre, String apellido, String contrasenna, String email, LocalDate fNacimiento) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.contrasenna = contrasenna;
+		this.email = email;
+		this.fNacimiento = fNacimiento;
+	}
+
+	public Usuario(long id, String nombre, String apellido, String contrasenna, String email, LocalDate fNacimiento) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.contrasenna = contrasenna;
+		this.email = email;
+		this.fNacimiento = fNacimiento;
+	}
+	//sets y gets
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getContrasenna() {
+		return contrasenna;
+	}
+
+	public void setContrasenna(String contrasenna) {
+		this.contrasenna = contrasenna;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getfNacimiento() {
+		return fNacimiento;
+	}
+
+	public void setfNacimiento(LocalDate fNacimiento) {
+		this.fNacimiento = fNacimiento;
+	}
+	//equals y hascode
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return id == other.id;
+	}
+	//To String
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", contrasenna=" + contrasenna
+				+ ", email=" + email + ", fNacimiento=" + fNacimiento + "]";
+	}
+	
+	
+}
