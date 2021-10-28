f06de00711937fcca3402d3c676aaabef9c17359
SAlejandr <alejitocastaneda1@gmail.com>
2021-10-28 21:12:21 +0200

    Pequeño cambio de css y continuando con el Controlador de Usuario

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,28 +1,52 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.pojos.Usuario;
 import com.example.demo.repository.UsuarioDao;
 
 @Controller
 @RequestMapping(value = "/usuario")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioDao dao;
-	
+
 	@GetMapping(value = "/login")
-	public ModelAndView getMethodName() {
-		ModelAndView mav =  new ModelAndView();
-		
+	public ModelAndView getLogin() {
+		ModelAndView mav = new ModelAndView();
+
 		mav.setViewName("logIn");
 		return mav;
 	}
 
+	@PostMapping(value = "/login")
+	public String postLogin(@RequestParam String usuario, @RequestParam String contrasenna) {
+		// TODO: process POST request
+		Optional<Usuario> persona = dao.findByEmail(usuario);
+		String redirectCorrecto = "redirect: /indesx", redirectIncorrecto = "redirect: /usuario/login";
+
+		if (persona.isPresent()) {
+			Usuario u = persona.get();
+
+			if (contrasenna.equals(u.getContrasenna()))
+				return redirectCorrecto;
+			else
+				return redirectIncorrecto;
+		} else {
+
+			return redirectIncorrecto;
+		}
+
+	}
 
 }
