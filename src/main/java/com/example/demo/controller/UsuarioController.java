9d81d0c12a425360fa2c13f47d4faae268081cf2
SAlejandr <alejitocastaneda1@gmail.com>
2021-10-29 18:13:20 +0200

    Probemos Usuarios

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,5 +49,31 @@ public class UsuarioController {
 		}
 
 	}
+	
+	@GetMapping(value = "/signup")
+	public ModelAndView getRegistrarse(@RequestParam String param) {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("signUp");
+		
+		return mav;
+	}
+
+	@PostMapping(value = "/signup")
+	public String postRegistrarse(@ModelAttribute Usuario usuario) {
+		//TODO: process POST request
+		Optional<Usuario> persona = dao.findByEmail(usuario.getEmail());
+		String redirectCorrecto = "redirect: /indesx", redirectIncorrecto = "redirect: /usuario/login";
+
+		if (!persona.isPresent()) {
+			dao.save(usuario);
+			
+			return redirectCorrecto;
+		} else {
+
+			return redirectIncorrecto;
+		}
+	}
+
 
 }
