a671a051f9b07ab1e9714885d1a12ff0d2820763
DAW <alejandroelguerra@gmail.com>
2021-11-10 19:39:01 +0100

    aaaaaa

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -181,6 +181,22 @@ public class UsuarioController {
 		return redirect;
 
 	}
+	@GetMapping(value = "/perfil/{id}/compras" )
+	public ModelAndView getcompra(@PathVariable long id,HttpSession session) {
+
+		Boolean logueado = (Boolean) session.getAttribute("logueado");
+		if(logueado == null) {
+			
+			logueado = false;
+		}
+		ModelAndView mav = new ModelAndView();
+
+		Usuario u = usuarioService.buscarPorId(id);
+		mav.addObject("usuario", u);
+		mav.addObject("logueado", logueado);
+		mav.setViewName("listaCompras");
+		return mav;
+	}
 
 
 }
