265c112223c54e80fef965c555a598a588696331
SAlejandr <alejitocastaneda1@gmail.com>
2022-01-13 19:38:55 +0100

    jaja si

---
+++ src/main/java/com/example/demo/repository/DaoGenerico.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import java.util.Map;
+
+public interface DaoGenerico<T> {
+
+	
+	long contarTodos(Map<String, Object> params);
+	
+	T crear(T t);
+	
+	void borrar(Object id);
+	
+	T buscar (Object id);
+	
+	T actualizar(T t);
+}
---
+++ src/main/java/com/example/demo/repository/DaoRepository.java
@@ -0,0 +1,37 @@
+package com.example.demo.repository;
+
+import java.util.Map;
+
+public class DaoRepository<T> implements DaoGenerico<T> {
+
+	@Override
+	public long contarTodos(Map<String, Object> params) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public T crear(T t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void borrar(Object id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T buscar(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T actualizar(T t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
