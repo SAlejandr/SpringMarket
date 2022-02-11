58628856a9478c7f9aaa6bf711bc5973a343d10f
SAlejandr <alejitocastaneda1@gmail.com>
2022-02-11 18:03:34 +0100

    Hold the Line (pam pam pam)

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -7,12 +7,17 @@ import java.util.Optional;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.pojos.Imagen;
@@ -115,8 +120,23 @@ public class MainController {
 		}
 	}
 	
-	
-	
+	@ResponseBody
+	@GetMapping(value = "/imagen/{id}")
+	public ResponseEntity getImageAsResponseEntity(@PathVariable String id) {
+
+		try {
+			Imagen imagesObj = imgServicio.encontrarImagen(Long.parseLong(id));
+			byte[] media = imagesObj.getImagen();
+			HttpHeaders headers = new HttpHeaders();
+			headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+
+			ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(media, headers, HttpStatus.OK);
+			return responseEntity;
+
+		} catch (Exception e) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+	}
 
 	@GetMapping(value = "/producto/{id}")
 	public String buscarProductoPorId(Model modelo, @PathVariable long id, HttpSession session) {
