73d764756cfea18c0f3e53eff38ec1f92c370978
SAlejandr <alejitocastaneda1@gmail.com>
2022-02-16 19:43:00 +0100

    It's not in the way you look or the things that you say that you do

--- src/main/java/com/example/demo/controller/ComentarioController.java
+++ src/main/java/com/example/demo/controller/ComentarioController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,8 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.dto.ComentarioDTO;
 import com.example.demo.pojos.Comentario;
 import com.example.demo.pojos.Producto;
+import com.example.demo.pojos.Usuario;
 import com.example.demo.service.IComentarioService;
 import com.example.demo.service.IProductoService;
+import com.example.demo.service.IUsuarioService;
 
 @RestController
 @RequestMapping("/comentario")
@@ -29,21 +32,26 @@ public class ComentarioController {
 	private IComentarioService service;
 	@Autowired
 	private IProductoService productoService;
+	@Autowired
+	private IUsuarioService usuarioService;
 	
 	@GetMapping(value = "/todos")
 	public List<ComentarioDTO> obtenerLista(@RequestParam Long producto) {
 		
 		Producto p = productoService.buscarPorId(producto);
 		
-		
 		List<ComentarioDTO> comentarios = new ArrayList<ComentarioDTO>();
+		
+		TreeMap<Long, Usuario> usuarios = new TreeMap<>();
+		
+		usuarioService.listarTodos().stream().forEach(u -> usuarios.put(u.getId(), u));
 		
 		service.obtenerComentariosPorProducto(p).stream().forEach(c -> {
 			
 			if(c.getComentarioPadre() != null) {
-				comentarios.add(new ComentarioDTO(c.getId(), c.getComentarioPadre().getId(), c.getTexto(), c.getUsuario().getNombre(), c.getFecha()));
+				comentarios.add(new ComentarioDTO(c.getId(), c.getComentarioPadre().getId(), c.getTexto(), usuarios.get(c.getUsuario().getId()).getNombre(), c.getFecha()));
 			}else {
-				comentarios.add(new ComentarioDTO(c.getId(), null, c.getTexto(), c.getUsuario().getNombre(), c.getFecha()));
+				comentarios.add(new ComentarioDTO(c.getId(), null, c.getTexto(), usuarios.get(c.getUsuario().getId()).getNombre(), c.getFecha()));
 			}
 			
 		});
