9eaa63b9a8722557e842b72ec8eda600b96a2b2e
DAW <DAW@DESKTOP-5OS3PHE>
2022-02-16 19:07:10 +0100

    aa

--- src/main/resources/static/js/ComentarioDTO.js
+++ src/main/resources/static/js/ComentarioDTO.js
@@ -1,24 +1,22 @@
-function Comentario(id,usuario,producto,comentarioPadre,texto){
-	this.id=id;
+function ComentarioDTO(usuario,producto,comentarioPadre,texto){
 	this.usuario=usuario;
 	this.producto=producto;
 	this.comentarioPadre=comentarioPadre;
 	this.texto=texto;
 	this.borrado=false;
 }
-function Comentario(id,usuario,producto,comentarioPadre,texto,borrado){
-	this.id=id;
+function ComentarioDTO(usuario,producto,comentarioPadre,texto,borrado){
 	this.usuario=usuario;
 	this.producto=producto;
 	this.comentarioPadre=comentarioPadre;
 	this.texto=texto;
 	this.borrado=borrado;
 }
-function Comentario(){
-	this.id=0;
+function ComentarioDTO(){
 	this.usuario=0;
 	this.producto=0;
 	this.comentarioPadre="";
 	this.texto="";
 	this.borrado=false;
 }
+
