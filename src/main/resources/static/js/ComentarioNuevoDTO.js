68519c6de2a1af4d202bb4b3ec1f25b0b2569b23
SAlejandr <alejitocastaneda1@gmail.com>
2022-02-18 17:53:32 +0100

    It's not in the way that your love set me free

--- src/main/resources/static/js/ComentarioNuevoDTO.js
+++ src/main/resources/static/js/ComentarioNuevoDTO.js
@@ -1,22 +1,20 @@
-function ComentarioDTO(usuario,producto,comentarioPadre,texto){
+function ComentarioNuevoDTO(usuario,producto,comentarioPadre,texto){
 	this.usuario=usuario;
 	this.producto=producto;
 	this.comentarioPadre=comentarioPadre;
 	this.texto=texto;
-	this.borrado=false;
 }
-function ComentarioDTO(usuario,producto,comentarioPadre,texto,borrado){
+function ComentarioNuevoDTO(usuario,producto,comentarioPadre,texto,){
 	this.usuario=usuario;
 	this.producto=producto;
 	this.comentarioPadre=comentarioPadre;
 	this.texto=texto;
 	this.borrado=borrado;
 }
-function ComentarioDTO(){
+function ComentarioNuevoDTO(){
 	this.usuario=0;
 	this.producto=0;
 	this.comentarioPadre=0;
 	this.texto="";
-	this.borrado=false;
 }
 
